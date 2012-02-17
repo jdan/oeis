b9c3d15c9f701616be83a6a9391564b67ec9a4b6
Jordan Scales <scalesjordan@gmail.com>
2012-02-17 01:52:43 -0500

    renamed to OEIS, implemented id, title, and sequence

--- lib/oeis.rb
+++ lib/oeis.rb
@@ -1,5 +1,32 @@
 require "oeis/version"
+require 'nokogiri'
+require 'open-uri'
 
-module Oeis
+module OEIS
   # Your code goes here...
+  class OEIS
+    
+    BASE_URL = 'http://oeis.org/search?q='
+    
+    attr_accessor :id, :title, :sequence
+    
+    def initialize(l)
+      url = BASE_URL + l.join(',')
+      doc = Nokogiri::HTML(open(url))
+      
+      first_result = doc.css('table table:eq(2)').first
+      info = first_result.css('tr:eq(3) table > tr > td > a').first
+      
+      @id = info.attributes['href'].value[1..-1]
+      @title = info.attributes['title'].value
+      
+      digits = first_result.css('tr:eq(4) table tr tt').text.split(', ')
+      @sequence = digits.map { |a| a.to_i }
+    end
+    
+  end
+  
+  def self.Search(l)
+    OEIS.new(l)
+  end
 end
