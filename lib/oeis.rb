ad7b7a7f9326e3bbd6eafce2549a1a7b681de14c
Ryan Davis <ryand-ruby@zenspider.com>
2012-02-21 13:39:07 -0800

    Stripped trailing whitespace

--- lib/oeis.rb
+++ lib/oeis.rb
@@ -5,11 +5,11 @@ require 'open-uri'
 module OEIS
   # Your code goes here...
   class OEISParser
-    
+
     BASE_URL = 'http://oeis.org/search?q='
-    
+
     attr_accessor :id, :title, :sequence
-    
+
     def initialize(ls)
       url = BASE_URL + ls.join(',')
       begin
@@ -17,28 +17,28 @@ module OEIS
       rescue SocketError
         puts 'Connection could not be established. Check your internet connection.'
       end
-      
+
       first_result = doc.css('table table:eq(2)').first
       info = first_result.css('tr:eq(3) table > tr > td > a').first
-      
+
       if info.nil?
         puts 'No sequence found.'
         return nil
       end
-      
+
       @id = info.attributes['href'].value[1..-1]
       @title = info.attributes['title'].value
-      
+
       digits = first_result.css('tr:eq(4) table tr tt').text.split(', ')
       @sequence = digits.map &:to_i
     end
-    
+
     def to_s
       "#{@id}: #{@title}"
     end
-    
+
   end
-  
+
   def self.search(ls)
     OEISParser.new(ls)
   end
